aa1444348e09776619f8fa43cc1c353717023e52
Brian Weston <brianw@webriculture.com>
2023-05-12 11:51:03 -0700

    Powershell for swapping static IPs

---
+++ powershell-scripts/static-ip-macore.ps1
@@ -0,0 +1,3 @@
+New-NetIPAddress -InterfaceIndex 5 -IPAddress 192.168.101.161 -PrefixLength 24 -DefaultGateway 192.168.101.254
+Set-DnsClientServerAddress -InterfaceIndex 5 -ServerAddresses 192.168.101.5, 172.16.10.5 
+Get-NetIPConfiguration -InterfaceIndex 5 
---
+++ powershell-scripts/static-ip-select.ps1
@@ -0,0 +1,3 @@
+New-NetIPAddress -InterfaceIndex 5 -IPAddress 172.16.100.7 -PrefixLength 16 -DefaultGateway 172.16.0.254
+Set-DnsClientServerAddress -InterfaceIndex 5 -ServerAddresses 172.16.10.5, 192.168.101.5
+Get-NetIPConfiguration -InterfaceIndex 5 
