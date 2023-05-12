c2d68b33b5818cfb4a7015ef97fa6c66a7bb9cfd
Brian Weston <brianw@webriculture.com>
2023-05-12 12:21:32 -0700

    WIP - Static IP powershell porblems
    Prior commit adds a new IP address, but doesn't remove the other one, if
    one is already set. Attempting to do that here. Haven't even begun 3rd
    setup, which is to remove any static IPs and turn DHCP on for when I'm
    home.
    
    Issues with this commit:
    * Doesn't want to remove DefaultGateway if one exists, so another gets
      added
    * Didn't seem to change the IP with respect to the GUI, at least.
      `Get-NetIPAddress -InterfaceIndex 5` reflects the change, however

--- powershell-scripts/static-ip-macore.ps1
+++ powershell-scripts/static-ip-macore.ps1
@@ -1,3 +1,4 @@
+Remove-NetIPAddress -InterfaceIndex 5 -PrefixOrigin Manual
 New-NetIPAddress -InterfaceIndex 5 -IPAddress 192.168.101.161 -PrefixLength 24 -DefaultGateway 192.168.101.254
 Set-DnsClientServerAddress -InterfaceIndex 5 -ServerAddresses 192.168.101.5, 172.16.10.5 
 Get-NetIPConfiguration -InterfaceIndex 5 
--- powershell-scripts/static-ip-select.ps1
+++ powershell-scripts/static-ip-select.ps1
@@ -1,3 +1,4 @@
+Remove-NetIPAddress -InterfaceIndex 5 -PrefixOrigin Manual
 New-NetIPAddress -InterfaceIndex 5 -IPAddress 172.16.100.7 -PrefixLength 16 -DefaultGateway 172.16.0.254
 Set-DnsClientServerAddress -InterfaceIndex 5 -ServerAddresses 172.16.10.5, 192.168.101.5
 Get-NetIPConfiguration -InterfaceIndex 5 
